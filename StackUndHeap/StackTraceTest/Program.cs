77becffd97e0df2268c77962e70c003941933ab9
Marcus Thomas <th@gso-koeln.de>
2019-05-06 11:19:41 +0200

    Stacktrace zeigt Namen der aufrufenden Methode.

---
+++ StackUndHeap/StackTraceTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace StackTraceTest
+{
+    class Program
+    {
+        static void TraceTest()
+        {
+            StackTrace st = new StackTrace();
+            string callerMethodName = st.GetFrame(1).GetMethod().Name;
+
+            Console.WriteLine("Aufrufende Methode: " + callerMethodName);
+        }
+
+        static void Main(string[] args)
+        {
+            TraceTest();
+
+            Console.ReadKey();
+        }
+    }
+}
