be21c7e767bbebb36f2a28201be27e4a9e9c1c1c
Marcus Thomas <th@gso-koeln.de>
2019-05-06 15:41:42 +0200

    Beispiel für String Value-Semantics hinzugefügt.

---
+++ StackSpielereien/StackSpielereien/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackSpielereien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = "Banane";
+            string b = a;
+            a = "Kirsche";
+
+            Console.WriteLine(b);
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+            int i = new int();
+            i = 5;
+            int j = new int();
+            j = i;
+            i = 4711;
+
+            Console.WriteLine(j);
+
+            Console.ReadKey();
+        }
+    }
+}
