fb51d6bfd762c8dc516ebb0dac9be3726232a675
Marcus Thomas <th@gso-koeln.de>
2019-05-06 11:24:30 +0200

    Quellenhinweis angegeben

--- StackUndHeap/StackTraceTest/Program.cs
+++ StackUndHeap/StackTraceTest/Program.cs
@@ -4,6 +4,12 @@ using System.Diagnostics;
 
 namespace StackTraceTest
 {
+    /// <summary>
+    /// Dieses Programm zeigt wie man die Bezeichnung der aufrufenden Methode
+    /// via (System.Diagnostics.)Stacktrace abfragen kann.
+    /// 
+    /// Quelle: Bayer, Das C# 2010 Codebook, S.1095
+    /// </summary>
     class Program
     {
         static void TraceTest()
